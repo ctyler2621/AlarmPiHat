e1d587d55bb4b7ec9ab74ae32470f2843d3c37b0
Chris Tyler <wisptech@gmail.com>
2021-06-30 12:43:34 -0500

    Make scripts executable

--- mkramdisk.sh
+++ mkramdisk.sh
@@ -2,7 +2,7 @@
 # Make a ram disk to spare the SD card
 if [[ ! -d "/home/pi/AlarmPiHat/ramdisk" ]]; then
   printf "[AlarmPiHat] Create ramdisk directory"
-  mkdir /home/pi/AlarmPiHat/ramdisk
+  sudo mkdir /home/pi/AlarmPiHat/ramdisk
 fi
 printf "[AlarmPiHat] Mounting Ram Disk"
 sudo mount -t tmpfs -o size=100m ramdisk /home/pi/AlarmPiHat/ramdisk
@@ -10,5 +10,8 @@ sudo chmod 777 /home/pi/AlarmPiHat/ramdisk
 
 # copy the AlarmPiHat database to the ram disk
 printf "[AlarmPiHat] Copy AlarmPiHat database to ram disk"
-cp /home/pi/AlarmPiHat/db/config.db /home/pi/AlarmPiHat/ramdisk/config.db
+sudo cp /home/pi/AlarmPiHat/db/config.db /home/pi/AlarmPiHat/ramdisk/config.db
 sudo chmod 777 /home/pi/AlarmPiHat/ramdisk/config.db
+
+# Make the poller executable
+sudo chmod +x /home/pi/AlarmPiHat/poller.php
