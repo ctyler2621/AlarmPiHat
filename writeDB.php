19ddf189d7eaa7ff55aa8daa44462b854e70380c
Chris Tyler <wisptech@gmail.com>
2021-06-30 12:12:27 -0500

    Syntax

--- writeDB.php
+++ writeDB.php
@@ -1,4 +1,4 @@
 #!/usr/bin/env php
 <?php
-phpexec("cp /home/pi/AlarmPiHat/ramdisk/config.db /home/pi/AlarmPiHat/db/config.db");
+exec("cp /home/pi/AlarmPiHat/ramdisk/config.db /home/pi/AlarmPiHat/db/config.db");
 ?>
