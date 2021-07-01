d84d51a4b4413e6c8cad25aa6e6159e6f3375b7d
Chris Tyler <wisptech@gmail.com>
2021-07-01 09:05:43 -0500

    Minor commit

--- poller.php
+++ poller.php
@@ -47,8 +47,9 @@ function storealarm($counter,$contact_alarm){
 function clearalarm($counter){
   // If there isn't currently and alarm then clear the contact(x)_alarm field in the database
   $contact_name = 'contact'.$counter.'_alarm';
+  $notification_name = 'notificaiton'.$counter;
   $pdo = new PDO('sqlite:/home/pi/AlarmPiHat/ramdisk/config.db');
-  $stm = $pdo->query("UPDATE config SET $contact_name=NULL WHERE 1");
+  $stm = $pdo->query("UPDATE config SET $contact_name=NULL $notification_name=NULL WHERE 1");
   $stm->execute();
 }
 
