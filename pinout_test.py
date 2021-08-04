9330e184b90d628ddfb433b32ebf1e90054e5aae
Chris Tyler <wisptech@gmail.com>
2021-08-04 09:36:33 -0500

    Pinout test
    RaPi 4 testing.

--- pinout_test.py
+++ pinout_test.py
@@ -24,17 +24,18 @@ values = [0 for a in range(40)]
 
 count = 1
 for value in values:
-  wiringpi.pullUpDnControl(count, 2) # Put the pin in pull down mode
-  wiringpi.pinMode(count, 0) # Set pin to INPUT
-  print("Pin:",count,"   Value:",wiringpi.digitalRead(count))
-  # Now set all pins to output and cycle them high and low for 1 second
-  wiringpi.pinMode(count, 1) # Set pin to OUTPUT
-  print("Pin", count, "going HIGH")
-  wiringpi.digitalWrite(count, 1) # Set pin HIGH
-  time.sleep(1)
-  print("Pin", count, "going LOW")
-  wiringpi.digitalWrite(count, 0) # Set pin LOW
-  time.sleep(1)
-  print("Reset pin to input")
-  wiringpi.pinMode(count, 0) # Set pin back to INPUT
+  if count != 28: # Pin 28 on the RasPi 4 locks up the device
+    wiringpi.pullUpDnControl(count, 2) # Put the pin in pull down mode
+    wiringpi.pinMode(count, 0) # Set pin to INPUT
+    print("Pin:",count,"   Value:",wiringpi.digitalRead(count))
+    # Now set all pins to output and cycle them high and low for 1 second
+    wiringpi.pinMode(count, 1) # Set pin to OUTPUT
+    print("Pin", count, "going HIGH")
+    wiringpi.digitalWrite(count, 1) # Set pin HIGH
+    time.sleep(1)
+    print("Pin", count, "going LOW")
+    wiringpi.digitalWrite(count, 0) # Set pin LOW
+    time.sleep(1)
+    print("Reset pin to input")
+    wiringpi.pinMode(count, 0) # Set pin back to INPUT
   count += 1
