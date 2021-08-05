670f16d066942758487adaad76f31cee75057653
Chris Tyler <wisptech@gmail.com>
2021-08-05 10:07:30 -0500

    New poller
    Syntax yet again

--- poller.py
+++ poller.py
@@ -24,6 +24,7 @@ def getData():
     # Create a list for values
     values_out = [4,3]
     values_in  = [27,0,1,24,28,29,22]
+    result = []
 
     counter = 0
     # Set input pins as inputs and put internal resistors into pulldown mode
