1c7c1246eb472540d5d7892c73e336840e50c1cf
Chris Tyler <wisptech@gmail.com>
2021-08-05 13:32:40 -0500

    New poller

--- poller.py
+++ poller.py
@@ -68,7 +68,7 @@ def getData():
     # Print the result for the end user just in case they are running it from the command line
     print()
     for key, value in result.items():
-        print(key,":",value)
+        print(key,":\t",value)
 
     # Return the result as a dictionary for ease of use later in the program
     return(result)
