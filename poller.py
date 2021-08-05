4176dc5de5591bb8cdde14cb3fcb213449ef644a
Chris Tyler <wisptech@gmail.com>
2021-08-05 12:06:29 -0500

    New Poller
    The poller is now polling all of the pins correctly and outputting the data in the correct format. However, contact 6 on the RasPi 4 doesn't work for some reason. It always reads as a low.
    Also added some comments to the program.

--- poller.py
+++ poller.py
@@ -22,50 +22,51 @@ def getData():
     # Set the IO function for wiringPi to use the BCM pinout
     wiringpi.wiringPiSetupGpio()
 
-    # Create a list for values using the BCM numbering
+    # Create a dictionary for values using the BCM numbering
     values_out = {"relay1":17,"relay2":4,"LED":21}
     values_in  = {"con1":26,"con2":16,"con3":19,"con4":13,"con5":12,"con6":6}
 
-    #Initialize the result variable as a list
+    # Initialize the result variable as a dictionary
     result = {"con1":0,"con2":0,"con3":0,"con4":0,"con5":0,"con6":0,"relay1":0,"relay2":0,"LED":0}
 
     # Set input pins as inputs and put internal resistors into pulldown mode
     for key, input in values_in.items():
-        wiringpi.pinMode(input, 0)         # Set pin to INPUT
-        wiringpi.pullUpDnControl(input, 1) # Put the pin in pull down mode
-        result.update({key:wiringpi.digitalRead(input)})
+        wiringpi.pinMode(input, 0)                        # Set BCM pin to INPUT
+        wiringpi.pullUpDnControl(input, 1)                # Put the BCM pin in pull down resistor mode
+        result.update({key:wiringpi.digitalRead(input)})  # Read the BCM pin
 
     # Set output pins as ouputs
     for key, output in values_out.items():
-        wiringpi.pinMode(output, 1)        # Set pin to OUTPUT
-        result.update({key:wiringpi.digitalRead(output)})
+        wiringpi.pinMode(output, 1)                       # Set pin to OUTPUT
+        result.update({key:wiringpi.digitalRead(output)}) # Read the BCM pin
 
-    # Print some output for debugging from the command line
+    # Print the result for the end user just in case they are running it from the command line
     print(result)
 
-    # Return the result as a list
+    # Return the result as a dictionary for ease of use later in the program
     return(result)
 
 def writeDb(result):
     # Write resulting data to the SQLite database on the ramdisk, everything will
     # reference the database so this shouldn't cause any issues with SNMP, etc.
-    print("Write to Database")
+    print()
+    print("Write data to the Database")
 
 def notifier(result):
     # If any value in the results is in an active state, send a notification
     # Base the time between notificaitons on the timer values in the database
     # Check the last notification datetime with current datetime and if x seconds
     # have not passed do not send the notification.
-    print("Notifier")
+    print()
+    print("Send notification via email if necessary")
 
 # Main code section
 try:
     while True:
         # Run a continuous loop and get the data every 5 seconds
-        result = getData()
-        #writeDb(result)
-        #notifier(result)
-        sleep(1)
-        result.clear()
+        result = getData()  # Get the data
+        writeDb(result)     # Write the data to the database
+        notifier(result)    # Send notificaiton email if necessary
+        sleep(5)            # Wait for x seconds
 except KeyboardInterrupt:
-    exit()
+    exit()                  # Exit the program if CTRL-C is pressed
