ca93c1df8486c61dcb9c67cdcd7e3d3c3a2690da
Chris Tyler <wisptech@gmail.com>
2021-09-22 11:58:54 -0500

    Test i2c

--- poller.py
+++ poller.py
@@ -160,7 +160,7 @@ try:
         writeDb(result)             # Write the data to the database
         #notifier(result)           # Send notificaiton email if necessary
         wiringpi.digitalWrite(6,0)  #Turn off the LED
-        sleep(10)                   # Wait for x seconds
+        sleep(2.5)                   # Wait for x seconds
         runcounter += 1             # Increment the runcounter
 except KeyboardInterrupt:
     exit()                  # Exit the program if CTRL-C is pressed
