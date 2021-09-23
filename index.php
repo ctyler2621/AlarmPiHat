0aac934bcd81d96bb65a1871f3d6086d9f1eb037
Chris Tyler <wisptech@gmail.com>
2021-09-23 10:57:50 -0500

    index
    Removed the user count

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ foreach($rows as $row){
 $data = shell_exec('uptime');
 $uptime = explode(' up ', $data);
 $uptime = explode(',', $uptime[1]);
-$uptime = $uptime[0].', '.$uptime[1];
+$uptime = $uptime[0];
 
 ?>
 
